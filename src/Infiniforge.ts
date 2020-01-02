9821fa51b90300da8be36953aac5e51acc0769a3
Shi Johnson-Bey <shijbey@gmail.com>
2020-01-02 15:08:23 -0500

    removed code from main repo

--- src/Infiniforge.ts
+++
@@ -1,6 +0,0 @@
-// This file exposes all of the source through a single module
-import * as Generator from './SwordGenerator';
-import * as CrossSections from './BladeCrossSections';
-import * as Templates from './SwordTemplates';
-
-export { Generator, CrossSections, Templates };
