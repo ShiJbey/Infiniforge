66d372f5741b787ee7e6c4494aca0a8370ad4a0a
Shi Johnson-Bey <shijbey@gmail.com>
2020-10-25 19:43:55 -0400

    removed unneeded export statement

--- src/Infiniforge.ts
+++ src/Infiniforge.ts
@@ -1,5 +1,3 @@
-import { SwordGenerator } from "./core/generators/sword/SwordGenerator";
-
 export * as SwordGenerator from "./core/generators/sword/SwordGenerator";
 export * as BladeCrossSection from './core/generators/sword/BladeCrossSection';
 export * as SwordTemplate from './core/generators/sword/SwordTemplate';
