fbaaa2a76fe1b1f522846b9abbe1cb339ea8b93c
Shi Johnson-Bey <shijbey@gmail.com>
2023-01-07 17:00:30 -0800

    added type declaration bundle, added package-lock

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,38 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import terser from "@rollup/plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
 
 export default [
-  {
-    input: "./src/infiniforge/index.ts",
-    output: [
-      {
-        file: "./dist/esm/infiniforge.js",
-        format: "esm",
-        sourcemap: true,
-      },
-      {
-        file: "./dist/cjs/infiniforge.js",
-        format: "cjs",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve({
-        // pass custom options to the resolve plugin
-        moduleDirectories: ["node_modules"],
-        preferBuiltins: false,
-      }),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-    ],
-    external: ["lodash", "three"],
-  },
+    {
+        input: './src/infiniforge/index.ts',
+        output: [
+            {
+                file: './dist/esm/infiniforge.module.js',
+                format: 'esm',
+                sourcemap: true,
+            },
+            {
+                file: './dist/cjs/infiniforge.js',
+                format: 'cjs',
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve({
+                // pass custom options to the resolve plugin
+                moduleDirectories: ['node_modules'],
+                preferBuiltins: false,
+            }),
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            terser(),
+        ],
+    },
+    {
+        input: './src/infiniforge/index.ts',
+        output: [{ file: 'dist/types.d.ts', format: 'esm' }],
+        plugins: [dts.default({ tsconfig: './tsconfig.json' })],
+    },
 ];
